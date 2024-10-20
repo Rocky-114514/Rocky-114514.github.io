f3aa9f9e0a07f74165f747b156647141f0beb740
Rocky-114514 <99902998+Rocky-114514@users.noreply.github.com>
2024-10-20 14:30:28 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 // scripts.js
+
+document.getElementById('convertButton').addEventListener('click', function() {
+	document.getElementById('postForm').submit();
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ul li a');
+	const navLinks = document.querySelectorAll('nav ul li a');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
+	navLinks.forEach(link => {
+		link.addEventListener('click', function(event) {
+			event.preventDefault();
+			const targetId = this.getAttribute('href').substring(1);
+			const targetSection = document.getElementById(targetId);
 
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+			if (targetSection) {
+				window.scrollTo({
+					top: targetSection.offsetTop,
+					behavior: 'smooth'
+				});
+			}
+		});
+	});
 });
